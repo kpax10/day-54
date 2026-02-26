c715c361652ee9d7a3e6bd67c2ebe0c472d0e33f
kyle <notxap55@yahoo.com>
2026-02-26 14:44:32 -0800

    completed

--- hello.py
+++ hello.py
@@ -1,11 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-print(__name__)
+# print(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
  
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import time
+current_time = time.time()
+print(current_time) # seconds since Jan 1st, 1970 
+
+# Write your code below 👇
+
+def speed_calc_decorator(func):
+  def wrapper():
+    first_time = time.time()
+    func()
+    second_time = time.time()
+    print(f"{func.__name__} run speed: {second_time - first_time}")
+  return wrapper
+
+@speed_calc_decorator
+def fast_function():
+  for i in range(1000000):
+    i * i
+        
+@speed_calc_decorator
+def slow_function():
+  for i in range(10000000):
+    i * i
+    
+fast_function()
+slow_function()
